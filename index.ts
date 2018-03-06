56ad6ca995534f81d09c1a717d7bc6983ad168a8
Álvaro Gómez <alvaro.gomez@grupoal.eu>
2018-03-06 13:01:49 +0100

    Back to normal server --> changing to pm2

--- index.ts
+++ index.ts
@@ -7,60 +7,43 @@ import {cpus} from 'os';
 //Own files imports
 import {MainServices} from "./src/rest-api/MainServices";
 
-const numCPUs = cpus().length;
 
-if (cluster.isMaster) {
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-    cluster.on("listening", function (worker) {
-        console.log("Cluster %d connected", worker.process.pid);
-    });
-    cluster.on("disconnect", function (worker) {
-        console.log("Cluster %d disconnected", worker.process.pid);
-    });
-    cluster.on("exit", function (worker) {
-        console.log("Cluster %d is dead", worker.process.pid);
-        // Ensuring a new cluster will start if an old one dies
-        cluster.fork();
-    });
-} else {
-    //Global variables declaration
-    let app: express.Application = express();
+//Global variables declaration
+let app: express.Application = express();
 
 //Global Router Declaration
-    let router: express.Router = express.Router();
+let router: express.Router = express.Router();
 
 //Handle Json Body
-    app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 // Logs directory
 
 //Enable CORS
-    app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "*");
-        res.header("Access-Control-Allow-Headers", "*");
-        next();
-    });
+app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 //Url context before services name
-    app.use('/api', router);
+app.use('/api', router);
 
 //Server configuration
-    let server = app.listen(3000, () => {
-        let host: string = server.address().address;
-        let port: number = server.address().port;
+let server = app.listen(3000, () => {
+    let host: string = server.address().address;
+    let port: number = server.address().port;
 
-        console.log(`App Listening at http://${host}:${port}`);
-    });
+    console.log(`App Listening at http://${host}:${port}`);
+});
 
 
 //Routes objects instantiation
 //let sumService = new MultiplyService(app);
-    new MainServices(router);
+new MainServices(router);
 
 //Export for testing purpose
-    module.exports = app;
-}
+module.exports = app;
+
 
